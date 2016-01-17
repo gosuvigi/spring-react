01eee9cc3753d249a344555c26b4da05ffedc2be
gosuvigi <gosuvigi@hotmail.com>
2016-01-17 18:38:19 +0100

    - add paging, create, delete, dialog

--- src/main/java/com/vigi/payroll/EmployeeRepository.java
+++ src/main/java/com/vigi/payroll/EmployeeRepository.java
@@ -1,11 +1,12 @@
 package com.vigi.payroll;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by vigi on 12/29/2015.
  */
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
